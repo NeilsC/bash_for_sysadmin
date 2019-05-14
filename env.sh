74511df6e2c7e0649c03e76b987c241a58e7ef94
Neils Christoffersen <nchristoffersen@essentialhub.com>
2019-05-14 15:25:35 -0500

    some scripts

---
+++ env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+clear
+
+echo "This script will give us environment information."
+echo "================================================="
+echo ""
+echo "Hello $USER"
+echo ""
+echo "Your home directory is $HOME"
+echo ""
+echo "Your terminal session type is $TERM"
+echo ""
+
---
+++ varexample.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MYUSERNAME="username"
+MYPASSWORD="password123"
+STARTOFSCRIPT=`date`
+
+echo "My login name is $MYUSERNAME"
+echo "My password is $MYPASSWORD"
+echo "Script started at $STARTOFSCRIPT"
+
+ENDOFSCRIPT=`date`
+
+echo "Script ended at $ENDOFSCRIPT"
